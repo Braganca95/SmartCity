88ae0e2d7bbb0a8dd53fd832f110d69540c7ea39
Braganca95 <39502790+Braganca95@users.noreply.github.com>
2021-03-17 20:09:26 +0000

    Esqueleto App v2

--- app/src/main/java/a23227/smartcity/MainActivity.kt
+++ app/src/main/java/a23227/smartcity/MainActivity.kt
@@ -1,5 +1,6 @@
 package a23227.smartcity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,5 +11,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun btnNotas(view: View) {}
+    fun btnNotas(view: View) {
+        val intent = Intent(this, Notas::class.java)
+        startActivity(intent)
+    }
 }
