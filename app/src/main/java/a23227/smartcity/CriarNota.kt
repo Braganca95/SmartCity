56f80c6d25eec37c004e68df19c43145763d2500
Braganca95 <39502790+Braganca95@users.noreply.github.com>
2021-03-18 20:06:41 +0000

    CriarNota v1

--- app/src/main/java/a23227/smartcity/CriarNota.kt
+++ app/src/main/java/a23227/smartcity/CriarNota.kt
@@ -1,14 +1,11 @@
 package a23227.smartcity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class CriarNota : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_criar_nota)
     }
-
 }
--- app/src/main/java/a23227/smartcity/Notas.kt
+++ app/src/main/java/a23227/smartcity/Notas.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import a23227.smartcity.Data.Nota
 import a23227.smartcity.adapters.NotaAdapter
+import android.content.Intent
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_notas.*
@@ -34,6 +35,12 @@ class Notas : AppCompatActivity() {
         val date = Date()
         notas.add(indice , Nota(view.titulo.toString(), view.rua.toString(), view.info.toString(), date))
         recyclerNotas.adapter?.notifyDataSetChanged()
+    }
+
+    fun CriarNota(view: View) {
+        val intent = Intent(this, CriarNota::class.java)
+        startActivity(intent)
+
     }
 
 }
